e4df4ffe03206e6abf8c1dc9fceb20120f436979
MarioAndrade <yomariolalo@hotmail.com>
2021-03-22 22:17:00 -0600

    Se agraga ejercicio 1 JS

---
+++ JS1/js/main.js
@@ -0,0 +1,103 @@
+//alert ("Hola koders!!")
+console.log("Hola Koders")
+
+// var variable1 = "Mario Andrade"
+// console.log(typeof variable1)
+// var variable2 = "Mario Andrade"
+// console.log(typeof variable1)
+// var variable3 = "Mario Andrade"
+// console.log(typeof variable1)
+
+// var num1 = 4
+// var num2 = 2
+// var suma
+// var resta
+// var multi
+// var div
+// var modulo
+// var increm
+// var decre
+// var increm2
+// var decre2
+
+// suma = num1 + num2;
+// resta = num1 - num2;
+// multi = num1 * num2;
+// div = num1 / num2;
+// modulo = num1 % num2;
+// increm = num1++;
+// increm2 = num2++;
+// decre = num1--;
+// decre2 = num2--;
+
+// document.writeln( "El resultado es" +suma+"\nEl resultado de la resta es" +resta +multi +div +modulo+ increm +increm2 +decre+ decre2+ "</p1>");
+
+
+// var a = prompt("Ingresa")
+// parseint
+// prompt
+
+
+
+
+
+// Ejercio 1
+// var lado1 = prompt("Ingresa el Lado 1 del triángulo")
+// var lado2 = prompt("Ingresa el Lado 2 del triángulo")
+// var lado3 = prompt("Ingresa el Lado 3 del triángulo")
+// var area;
+
+
+// if((lado1 != lado2) && (lado1!= lado3) && (lado2!= lado3)){
+//     alert ("El triángulo es Escaleno")
+    
+    
+// }
+// if(((lado1 == lado2) && (lado2!= lado3))||((lado2 == lado3) && (lado2!= lado1))||((lado1 == lado3) && (lado1!= lado2))){
+//     alert ("El triángulo es Isósceles")
+    
+// }
+// if((lado1 == lado2) && (lado1== lado3) && (lado2 == lado3)){
+//     alert ("El triángulo es Equilatero")
+    
+    
+// }
+
+
+
+
+
+
+// var lado1 = prompt("Ingresa el Lado 1 del cuadrilátero")
+// var lado2 = prompt("Ingresa el Lado 2 del cuadrilátero")
+// var area;
+
+
+// if  (lado1 == lado2){
+
+//     area = lado1*lado2;
+//     alert ("El cuadrilátero es un cuadrado")
+//     alert ("El area del cuadrado es: "+ area)
+    
+// }
+// if(lado1 != lado2){
+//     area = lado1*lado2;
+//     alert ("El cuadrilátero es un rectangulo")
+//     alert ("El area del rectangulo es: "+ area)
+    
+// }
+
+
+
+
+
+var base = prompt("Ingresa el base  del triángulo")
+var altura = prompt("Ingresa el altura  del triángulo")
+var area
+area= (base*altura)/2;
+alert ("El area del triángulo es: "+ area)
+
+
+
+
+
