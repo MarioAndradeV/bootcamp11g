e7df2e53f14d3cbc50609b0b9436c5de40c7e1a6
MarioAndrade <yomariolalo@hotmail.com>
2021-03-24 15:28:24 -0600

    Se agrega Ejercicio 2 JS

--- Ejercicio2_JS/JS/main.js
+++ Ejercicio2_JS/JS/main.js
@@ -1,3 +1,89 @@
+// Ejercicio 1 //
+
+// let weight = parseInt(prompt("Ingresa tu peso en Kilogramos"));
+// let height = parseFloat(prompt("Ingresa tu altura en metros"));
+// let bodyComposition;
+// let IMC = weight / (Math.pow(height, 2));
+// IMC = IMC.toFixed(1);
+// if(IMC < 18.5){
+//     bodyComposition = "Peso inferior al normal, IMC menos a 18.5"
+// } else if(IMC >= 18.5 && IMC <= 24.9){
+//     bodyComposition = "Normal, IMC 18.5 - 24.9"
+// } else if(IMC >= 25.0 && IMC <= 29.9){
+//     bodyComposition = "Peso superior al normal, IMC 25.0 - 29.9"
+// } else if(IMC >= 30.0){
+//     bodyComposition = "Obesidad, IMC mayor a 30.0"
+// }
+// console.log(`Tu IMC es de ${IMC} - ${bodyComposition}`);
+
+
+
+//Ejercicio 2 //
+let nationality = prompt("Introduce el nombre de tu país de Origen");
+let gender = prompt("Introduce tu género");
+let height = parseFloat(prompt("Introduce tu altura en centímetros"));
+let heightAverage;
+switch (nationality){
+    case 'México':
+        heightAverage = gender === "masculino" ?  170 : 160.8;
+        if(height < heightAverage){
+            console.log("Tu altura es menor al promedio");
+        } else if(height > heightAverage){
+            console.log("Tu altura es mayor al promedio");
+        } else{
+            console.log("Tu altura es promedio");
+        }
+        break;
+    case 'Australia':
+        heightAverage = gender === "masculino" ?  179.2 : 165.9;
+        if(height < heightAverage){
+            console.log("Tu altura es menor al promedio");
+        } else if(height > heightAverage){
+            console.log("Tu altura es mayor al promedio");
+        } else{
+            console.log("Tu altura es promedio");
+        }
+        break;
+    case 'Canadá':
+        heightAverage = gender === "masculino" ?  178.1 : 163.9;
+        if(height < heightAverage){
+            console.log("Tu altura es menor al promedio");
+        } else if(height > heightAverage){
+            console.log("Tu altura es mayor al promedio");
+        } else{
+            console.log("Tu altura es promedio");
+        }
+        break;
+    case 'Brasil':
+        heightAverage = gender === "masculino" ?  173.6 : 160.9;
+        if(height < heightAverage){
+            console.log("Tu altura es menor al promedio");
+        } else if(height > heightAverage){
+            console.log("Tu altura es mayor al promedio");
+        } else{
+            console.log("Tu altura es promedio");
+        }
+        break;
+    case 'Reino Unido':
+        heightAverage = gender === "masculino" ?  177.5 : 164.4;
+        if(height < heightAverage){
+            console.log("Tu altura es menor al promedio");
+        } else if(height > heightAverage){
+            console.log("Tu altura es mayor al promedio");
+        } else{
+            console.log("Tu altura es promedio");
+        }
+        break;
+    default:
+        console.log("El país ingresado no se encuentra en nuestra base de datos");
+        break;
+}
+
+
+
+
+
+
 //Ejercicio 3 //
 
 var lado = prompt("Ingresa el lado del cubo en metros")
@@ -60,4 +146,4 @@ switch(material){
         alert ("No hay datos de la densidad de ese material")    
 }
 
-//alert ("El area del triángulo es: "+ area)
+
---
+++ Ejercicio3_JS_String/JS/main.js
@@ -0,0 +1,24 @@
+var phrase = "La mejor forma de predecir el futuro es creándolo"
+
+var num = phrase.length;
+var numa = phrase.match(/[AaÁá]/gi);
+var numaTotal= numa.length;
+
+var palabraPar;
+var palabraImp;
+
+
+
+
+
+console.log( ` Existen ${numaTotal} a `) 
+
+for( var i = 0; i< num; i+=2 ){
+    palabraPar += phrase.charAt(i);  
+     
+   
+}
+console.log(palabraPar)
+
+
+
